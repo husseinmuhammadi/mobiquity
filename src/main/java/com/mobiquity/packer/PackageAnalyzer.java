0e0405183ec60edbf7a2d58db9e92451fb040810
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-06-07 15:20:38 +0430

    BugFix: Correct packing result

--- src/main/java/com/mobiquity/packer/PackageAnalyzer.java
+++ src/main/java/com/mobiquity/packer/PackageAnalyzer.java
@@ -4,10 +4,13 @@ import com.mobiquity.model.Knapsack;
 import com.mobiquity.model.KnapsackItem;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class PackageAnalyzer implements Observer {
 
@@ -23,9 +26,11 @@ public class PackageAnalyzer implements Observer {
 
         KnapsackAlgorithm knapsackAlgorithm = new KnapsackAlgorithm(knapsack.getLimit(), knapsack.getItems().toArray(KnapsackItem[]::new));
         List<KnapsackItem> result = knapsackAlgorithm.calc();
+        result.sort(Comparator.comparingInt(KnapsackItem::getIndex));
+        List<Integer> index = result.stream().map(KnapsackItem::getIndex).collect(Collectors.toList());
 
         if (!result.isEmpty())
-            onFindResult.accept(StringUtils.join(result, ","));
+            onFindResult.accept(StringUtils.join(index, ","));
         else
             onFindResult.accept("-");
     }

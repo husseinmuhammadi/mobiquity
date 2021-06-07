227d31bd4996c41c04e56740874445b8855cf076
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-06-07 13:58:08 +0430

    Add new class for calculating knapsack problem

---
+++ src/main/java/com/mobiquity/packer/KnapsackAlgorithm.java
@@ -0,0 +1,19 @@
+package com.mobiquity.packer;
+
+import com.mobiquity.model.KnapsackItem;
+
+import java.util.Collections;
+import java.util.List;
+
+public class KnapsackAlgorithm {
+
+    private final KnapsackItem[] items;
+
+    public KnapsackAlgorithm(KnapsackItem[] items) {
+        this.items = items;
+    }
+
+    public List<KnapsackItem> calc(double W, int n) {
+        return Collections.emptyList();
+    }
+}
